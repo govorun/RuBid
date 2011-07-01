63a384d8ab62cd88bb6f9cf6f1f4087c7be46d00
Mike Ivanov <daekrist@gmail.com>
2011-07-01 20:15:01 +0700

    initial Rails 3.1 commit, application created

---
+++ rorapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rorapp::Application.config.secret_token = '4025985bc7e36676c8fefb6d4e437c21c34e8fd6e03dac59d5c7b7c0a8afc89a42c002331b16b8e3bd179ca561d0e2880b6edefffe246da054762a79438e754b'
